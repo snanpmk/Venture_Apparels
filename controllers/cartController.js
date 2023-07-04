e761cfd40ec204280e8a77a61961ec88cc5c3140
Sinan <pmksinan@gmail.com>
2023-07-04 20:15:44 +0530

    total price based on the quantity

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,53 +1,68 @@
 const Cart = require("../models/cartModel");
 const Product = require("../models/productModel");
+
 const viewCart = async function (req, res) {
-    try {
-      const userId = req.session.userId;
-  
-      const cart = await Cart.findOne({ userId: userId }).populate('items.productId');
-      const products = cart.items.map(item => item.productId);
-  
-      res.render("cart", { layout: "layouts/userLayout", products: products, cart:cart });
-    } catch (err) {
-      console.log("error in viewing cart", err);
-    }
-  };
-  
+  try {
+    const userId = req.session.userId;
+
+    const cart = await Cart.findOne({ userId: userId }).populate(
+      "items.productId"
+    );
+    const products = cart.items.map((item) => item.productId);
+    cart.items = cart.items.map((item) => {
+      const productPrice = item.productId.price;
+      const totalPrice = productPrice * item.quantity;
+      item.totalPrice = totalPrice; // Update totalPrice in the item
+      console.log(item.totalPrice);
+      return item;
+    });
+    console.log(cart.items);
+    await cart.save(); // Save the updated cart
+    res.render("cart", {
+      layout: "layouts/userLayout",
+      products: products,
+      cart: cart,
+      item: cart.items,
+    });
+  } catch (err) {
+    console.log("error in viewing cart", err);
+  }
+};
+
 const addToCart = async function (req, res) {
-    try {
-      const userId = req.session.userId;
-      const productId = req.params.ObjectId;
-  
-      console.log(userId + "❤️❤️❤️" + productId);
-  
-      let cart = await Cart.findOne({ userId: userId });
-      if (!cart) {
-        cart = await Cart.create({ userId });
-      }
-  
-      const existingItem = cart.items.find(
-        (item) => item.productId.toString() === productId
-      );
-      if (existingItem) {
-        // Increase the quantity by 1 when viewing the cart
-        existingItem.quantity += 1;
-      } else {
-        // If the product doesn't exist, add a new item to the cart
-        cart.items.push({ productId, quantity: 1 }); // Initialize quantity to 1 for new items
-      }
-  
-      await cart.save();
-      res
-        .status(200)
-        .json({ message: "Product added to cart successfully", cart });
-    } catch (err) {
-      console.log("error in adding to the cart", err);
-      res
-        .status(500)
-        .json({ error: "An error occurred while adding the product to cart" });
+  try {
+    const userId = req.session.userId;
+    const productId = req.params.ObjectId;
+
+    console.log(userId + "❤️❤️❤️" + productId);
+
+    let cart = await Cart.findOne({ userId: userId });
+    if (!cart) {
+      cart = await Cart.create({ userId });
     }
-  };
-  
+
+    const existingItem = cart.items.find(
+      (item) => item.productId.toString() === productId
+    );
+    if (existingItem) {
+      // Increase the quantity by 1 when viewing the cart
+      existingItem.quantity += 1;
+    } else {
+      // If the product doesn't exist, add a new item to the cart
+      cart.items.push({ productId, quantity: 1 }); // Initialize quantity to 1 for new items
+    }
+
+    await cart.save();
+    res
+      .status(200)
+      .json({ message: "Product added to cart successfully", cart });
+  } catch (err) {
+    console.log("error in adding to the cart", err);
+    res
+      .status(500)
+      .json({ error: "An error occurred while adding the product to cart" });
+  }
+};
 
 module.exports = {
   viewCart,
--- models/cartModel.js
+++ models/cartModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { Number } = require('twilio/lib/twiml/VoiceResponse');
 
 const cartSchema = new mongoose.Schema({
   userId: {
@@ -16,6 +17,11 @@ const cartSchema = new mongoose.Schema({
       type: Number,
       required: true,
       default: 1
+    },
+    totalPrice :{
+      type:Number,
+      default: 0,
+      
     }
   }],
   createdAt: {
