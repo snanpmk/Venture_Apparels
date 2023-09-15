5a55dc3e0d7e835e72ee0f382744b1e9abc7dfc4
Sinan <pmksinan@gmail.com>
2023-09-15 08:15:51 +0530

    third commit

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const state = {
 };
 
 module.exports.connect = async function () {
-  const url = "mongodb+srv://sinanpmk:sinanmongoatlas1526@cluster0.xhcyklv.mongodb.net/";
+  const url = "mongodb+srv://sinanpmk:sinanmongoatlas1526@cluster0.xhcyklv.mongodb.net";
   const dbname = "venture_apparel";
 
   try {
