257fc7fbe1aa310805ae7446e8e514c9bba65221
Sinan <pmksinan@gmail.com>
2023-09-15 08:20:44 +0530

    third commit

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const state = {
 
 module.exports.connect = async function () {
   const url = "mongodb+srv://sinanpmk:sinanmongoatlas1526@cluster0.xhcyklv.mongodb.net";
-  const dbname = "venture_apparel";
+  const dbname = "venture_apparels";
 
   try {
     await mongoose.connect(url + "/" + dbname, {
